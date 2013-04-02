008eec8605c4e3d011f8e7582d826eb9ec5b058f
teresam <teresamcav@sbcglobal.net>
2013-04-01 19:18:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,61 +1,54 @@
 <?php
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('WWW-Authenticate: Basic realm="Administrator Only"');
     header('HTTP/1.0 401 Unauthorized');
-  echo("<script>location.href='/CMWEB241/CMWEB241_Lab06/index.php'</script>");
+	include('layouts/footer.php');	
 		//log file info
   		$file='admin/unauthorized.txt';
-  		$address = $_SERVER['REMOTE_ADDR'];
+  		$address = $_SERVER['REMOTE_ADDR'];		
  		$timestamp = strftime("%Y-%m-%d, %H:%M",time());
 		$text = "attempted access on ";
 		$content = $text .$timestamp;
   		$handle=fopen($file,'a') or die("Can't open file");
- 		fwrite($handle, "$address .$content\n");
+ 		fwrite($handle, "$address $content\n");
  		fclose($handle);
-		exit();	   
-} else {
+		exit();	 
+		echo("<script>location.href='/CMWEB241_Lab08/public/index.php'</script>");  
+} 
+else  {
 	if(($_SERVER['PHP_AUTH_PW'] == 'P@ssw0rd!') && ($_SERVER['PHP_AUTH_USER'] == 'CMWEB241')){
-    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
-   // echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+    echo "Hello {$_SERVER['PHP_AUTH_USER']}.";
+    echo "You are being redirected to the login page.";
+	echo ("<script>location.href='/CMWEB241_Lab08/admin/admin.php'</script>");
 		//log file info
-		$file='admin/access.txt';
+		$file='admin/authorized.txt';
   		$address = $_SERVER['REMOTE_ADDR'];
  		$timestamp = strftime("%Y-%m-%d, %H:%M",time());
 		$text1 = "was granted access on ";
 		$content1 = $text1 .$timestamp;
   		$handle=fopen($file,'a');
- 		fwrite($handle, "$address .$content1\n");
-  		fclose($handle);
-		echo ("<script>location.href='/CMWEB241/CMWEB241_Lab06/admin/admin.php'</script>");
-}
-	else {
-		echo("<script>location.href='/CMWEB241/CMWEB241_Lab06/index.php'</script>");
-			//log file info
-  		$file='../unauthorized.txt';
-  		$address = $_SERVER['REMOTE_ADDR'];
- 		$timestamp = strftime("%Y-%m-%d, %H:%M",time());
-		$text = "attempted access on ";
-		$content = $text .$timestamp;
-  		$handle=fopen($file,'a') or die("Can't open file");
- 		fwrite($handle, "$address .$content\n");
- 		fclose($handle);
-		exit();	   
+		fwrite($handle, "$address $content1\n");
+		fclose($handle);
+		exit();
+		
+		
 	}
 }
-?>
 
+
+?>
 <?php
-	$array['../images/branch.jpg']['caption'] = 'Frost on bush';
-	$array['../images/falls.jpg']['caption'] = 'Black Eagle Falls, MT';
-	$array['../images/frozenweb.jpg']['caption'] = 'Spider web';
-	$array['../images/ice.jpg']['caption'] = 'Ice on window';
-	$array['../images/mountain.jpg']['caption'] = 'Montana Mountain';	
+	$array['images/branch.jpg']['caption'] = 'Frost on bush';
+	$array['images/falls.jpg']['caption'] = 'Black Eagle Falls, MT';
+	$array['images/frozenweb.jpg']['caption'] = 'Spider web';
+	$array['images/ice.jpg']['caption'] = 'Ice on window';
+	$array['images/mountain.jpg']['caption'] = 'Montana Mountain';	
 ?>
 
 <html> 
  <head>
 	<title>Photo Gallery</title>
-	<link rel ="stylesheet" type="text/css" href="../stylesheets/main.css" media="all" />
+	<link rel ="stylesheet" type="text/css" href="stylesheets/main.css" media="all" />
  </head>
  <body>
    <div id='header'>
