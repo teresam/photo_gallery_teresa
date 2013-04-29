a1dcfba4e68064445dce24a5bd75c6c6aafa997b
teresam <teresamcav@sbcglobal.net>
2013-04-29 18:17:18 -0500

    Update deleteAuthorized.php

--- admin/deleteAuthorized.php
+++ admin/deleteAuthorized.php
@@ -1,19 +1,28 @@
+<?php
+session_start();
+if(!$_SESSION['authorized']){
+		header("location:form.php");
+}else if($_SESSION['authorized']){ 
+	  echo "<a href='../admin/admin.php'>Admin</a>	";
+	  } 
+?>
+
 <html> 
 <head>
  <title>Photo Gallery</title>
- <link rel ="stylesheet" type="text/css" href="../stylesheets/main.css" media="all" />
+ <link rel ="stylesheet" type="text/css" href="stylesheets/main.css" media="all" />
 </head>
 <body>
 
 <div id='header'>
-  <?php include('../layouts/header_admin.php'); ?>
+	<?php include('layouts/header_admin.php'); ?>
 </div>
 
 <div id="main">
 	<h2>Delete Authorized Access Log file</h2>
 	<ul>
 	<li><a href='admin.php'>Main Administration Page</a></li>
-	<li><a href='../index.php'>Return to Gallery</a></li>
+	<li><a href='../public/index.php'>Return to Gallery</a></li>
 	</ul>
 
 <?php
@@ -31,7 +40,7 @@ echo $text, $curDate;
 ?>
 </div>
 <div id='footer'>
-	<?php include('../layouts/footer_admin.php'); ?>
+	<?php include('layouts/footer_admin.php'); ?>
 </div>
 </body>
 </html>
